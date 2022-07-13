517451bb4eab78aa3efa9c5dab1acf9fbc939c48
DiegoHerbas <d.herbas@hotmail.com>
2022-07-13 00:40:08 -0400

    notificacion

--- lib/helper/push_notifications_services.dart
+++ lib/helper/push_notifications_services.dart
@@ -1,4 +1,49 @@
 //
 //SHA1: EE:72:E7:8B:3E:4D:C4:65:7E:9B:AD:5E:6A:C9:33:D3:BC:FC:B8:AA
 
-class PushNotificationsService {}
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+  static FirebaseMessaging messaging = FirebaseMessaging.instance;
+  static String? token;
+  static StreamController<String> _messageStream = StreamController.broadcast();
+  static Stream<String> get messageStream => _messageStream.stream;
+
+  static Future _backgroundhandler(RemoteMessage message) async {
+    //print('onbackground Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['producto'] ?? 'no data');
+  }
+
+  static Future _onMessagehandler(RemoteMessage message) async {
+    // print('onMessage Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['producto'] ?? 'no data');
+  }
+
+  static Future _onMessageopenApp(RemoteMessage message) async {
+    //  print('onMessageOpenApp Handler ${message.messageId}');
+    print(message.data);
+    _messageStream.add(message.data['producto'] ?? 'no data');
+  }
+
+  static Future initializeApp() async {
+    // push notificatios
+    await Firebase.initializeApp();
+    token = await FirebaseMessaging.instance.getToken();
+    print('Token: $token');
+
+    FirebaseMessaging.onBackgroundMessage(_backgroundhandler);
+    FirebaseMessaging.onMessage.listen(_onMessagehandler);
+    FirebaseMessaging.onMessageOpenedApp.listen(_onMessageopenApp);
+
+    //local notification
+  }
+
+  static closeStreams() {
+    _messageStream.close();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_listas/helper/push_notifications_services.dart';
 import 'package:flutter_application_listas/modelo/dispositivosModel.dart';
 import 'package:flutter_application_listas/provider/TemperaturaProvider.dart';
 import 'package:flutter_application_listas/provider/dispositivoProvider.dart';
@@ -10,9 +12,12 @@ import 'package:flutter_application_listas/views/home_page2.dart';
 import 'package:flutter_application_listas/views/message_screen.dart';
 import 'package:flutter_application_listas/views/screens.dart';
 import 'package:provider/provider.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await PushNotificationService.initializeApp();
+  runApp(MyApp());
 }
 
 class AppState extends StatelessWidget {
@@ -33,8 +38,49 @@ class AppState extends StatelessWidget {
   }
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final GlobalKey<NavigatorState> navigatorKey =
+      new GlobalKey<NavigatorState>();
+  final GlobalKey<ScaffoldMessengerState> messengerKey =
+      new GlobalKey<ScaffoldMessengerState>();
+
+  @override
+  void initState() {
+    super.initState();
+    //context
+    PushNotificationService.messaging
+        .getInitialMessage()
+        .then((RemoteMessage? message) {
+      if (message != null) {
+        final snackBar = SnackBar(content: Text(message.data['producto']));
+        messengerKey.currentState?.showSnackBar(snackBar);
+        navigatorKey.currentState
+            ?.pushNamed('message', arguments: message.data['producto']);
+      }
+    });
+
+    //context
+    PushNotificationService.messageStream.listen((message) {
+      navigatorKey.currentState?.pushNamed('message', arguments: message);
+      final snackBar = SnackBar(content: Text(message));
+      messengerKey.currentState?.showSnackBar(snackBar);
+    });
+
+    /* PushNotificationService.messageStream.listen((message) {
+      // print('MyApp:$message');
+      navigatorKeys.currentState?.pushNamed('message', arguments: message);
+
+      final snackBar = SnackBar(content: Text(message));
+      messengerKey.currentState?.showSnackBar(snackBar);
+    });*/
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,10 +88,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'LISTAS',
       initialRoute: 'homeNoti',
+      navigatorKey: navigatorKey, //navegar
+      scaffoldMessengerKey: messengerKey, //sancks
       routes: {
         'homePage2': (_) => DispositivoHome(),
-	'homeNoti': (_) => HomeNotificacion(),
-        'Message': (_) => MessageScreen(),
+        'homeNoti': (_) => HomeNotificacion(),
+        'message': (_) => MessageScreen(),
       },
     );
   }
